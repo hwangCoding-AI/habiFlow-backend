c30c6980a04bf7eea754c994461f0cbf9bfc0684
hyungrae.hwang <140670934+hwangCoding-AI@users.noreply.github.com>
2025-06-16 10:49:43 +0900

    Add files via upload

---
+++ habiFlow-backend/src/main/java/com/habiflow/backend/HabiflowBackendApplication.java
@@ -0,0 +1,11 @@
+package com.habiflow.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabiflowBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HabiflowBackendApplication.class, args);
+    }
+}
---
+++ habiFlow-backend/src/main/java/com/habiflow/backend/domain/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.habiflow.backend.domain.user;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+    Mono<User> findByUsername(String username);
+}
